e24429a978788bb3628301836345058d4a9f485c
p3dr4m <seenkim@gmail.com>
2018-03-19 16:31:35 -0700

    commit again

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 const request = require('request');
 
 let app = express();
 
+
+
+hbs.registerPartials(__dirname + '/views/partials');
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (request, response) => {
-   response.send()
+    response.render('main.hbs', {
+        title: 'Login'
+    })
+});
+
+app.get('/app', (request, response) => {
+    response.render('app.hbs', {
+        title: 'App'
+    })
 });
 
 app.listen(8080, () => {
---
+++ secret.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+let secret = () => {
+    return new Promise((resolve, reject) => {
+        if (typeof lat === 'number' && typeof lon === 'number') {
+            request({
+                url: 'https://api.themoviedb.org/3/movie/550?api_key=686d996b7a847930b2a9d18cec90cad3'
+                json: true
+            }, (error, response, body) => {
+                if (!error && response.statusCode === 200) {
+                    let obj = {
+                        'place': place,
+                        'temp': body.currently.temperature,
+                        'summary': body.currently.summary
+                    };
+                    resolve(obj)
+                } else {
+                    reject(body.error);
+                }
+            })
+        } else {
+            reject('REJECTED: Not numbers')
+        }
+    })
+};
+
+modules.exports = {
+    secret
+};
