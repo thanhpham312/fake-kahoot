07202c8f1f49632bf2c7447150d4839f4cee2429
thanhpham312 <tpham74@my.bcit.ca>
2018-03-28 14:29:20 -0700

    Changed the look.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -13,7 +13,7 @@ var displayQuestion = () => {
     xmlhttp.open("GET","/", true);
     xmlhttp.onreadystatechange = () =>{
         if (xmlhttp.readyState==4 && xmlhttp.status==200){
-            document.getElementById('questionNumber').innerHTML = 'Question ' + currentQuestion.toString();
+            document.getElementById('questionNumber').innerHTML = 'QUESTION ' + currentQuestion.toString();
         }
     }
     xmlhttp.send();
