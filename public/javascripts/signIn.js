18ad6e463d7a04b1f5507d8818df77421b33d00f
aiibaby <gzq1030@yahoo.com>
2018-05-03 17:30:21 -0700

    create signIn box

---
+++ public/javascripts/signIn.js
@@ -0,0 +1,21 @@
+var signInPage = document.getElementById('signInPage')
+var signInButton = document.getElementById('signInButton')
+var signInPage2 = document.getElementById('signInPage2')
+var signInPage3 = document.getElementById('signInPage3')
+var signInPage4 = document.getElementById('signInPage4')
+
+signInButton.addEventListener('click', function () {
+  signInPage.style.opacity = 1;
+})
+
+signInButton.addEventListener('click', function () {
+  signInPage2.style.opacity = 1;
+})
+
+signInButton.addEventListener('click', function () {
+  signInPage3.style.opacity = 1;
+})
+
+signInButton.addEventListener('click', function () {
+  signInPage4.style.opacity = 1;
+})
