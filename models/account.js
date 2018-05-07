863e71944409ff23910cf97928e519c412d8899d
ishalagin <i.shalagin@gmail.com>
2018-05-06 19:38:33 -0700

    added hashing for registration, added half the login function

--- models/account.js
+++ models/account.js
@@ -15,7 +15,20 @@ class Account {
    * @returns {undefined}
    */
   login (username, password) {
-    return undefined
+    return new Promise((resolve, reject) => {
+      this.encryptPassword(password).then((result) => {
+        db.executeQuery(`SELECT * FROM public."ACCOUNTS" WHERE "USERNAME" = '${username}' AND "PASSWORD" = '${result}';`).then((queryResult) => {
+          if (queryResult.length > 0) {
+            this.username = queryResult[0].USERNAME
+            this.password = queryResult[0].PASSWORD
+            this.userID = queryResult[0].ACCOUNT_ID
+            resolve(true)
+          } else {
+            reject(false)
+          }
+        })
+      })
+    })
   }
 
   encryptPassword (password) {
@@ -29,42 +42,40 @@ class Account {
    * @returns {undefined}
    */
   register (username, password) {
-    return new Promise((resolve,reject) => {
-      db.executeQuery(`INSERT INTO public."ACCOUNTS"("USERNAME", "PASSWORD") VALUES ('${username}', '${password}');`).then((result) =>{
-        resolve(result)
+    return new Promise((resolve, reject) => {
+      this.encryptPassword(password).then((result) => {
+        db.executeQuery(`INSERT INTO public."ACCOUNTS"("USERNAME", "PASSWORD") VALUES ('${username}', '${result}');`).then((result) => {
+          resolve(result)
+        })
       })
     })
   }
 
-
   validateUsername (USERNAME) {
-    return new Promise((resolve,reject) => {
+    return new Promise((resolve, reject) => {
       db.executeQuery('SELECT "USERNAME" FROM "ACCOUNTS"').then((result) => {
         console.log(result)
-        let user_array = JSON.parse(result)
-        var found = user_array.some(function (el) {
-          return el.USERNAME === USERNAME;
-        });
+        let userArray = JSON.parse(result)
+        var found = userArray.some(function (el) {
+          return el.USERNAME === USERNAME
+        })
         resolve(!found)
       })
-      
     })
   }
 
   validatePassword (pass) {
-    var numbers = pass.match(/\d+/g);
-    var uppers  = pass.match(/[A-Z]/);
-    var lowers  = pass.match(/[a-z]/);
-    var lengths = pass.length>=6;
-    var valid = undefined;
+    var numbers = pass.match(/\d+/g)
+    var uppers = pass.match(/[A-Z]/)
+    var lowers = pass.match(/[a-z]/)
+    var lengths = pass.length >= 6
+    var valid = undefined
 
-    if (numbers === null || uppers === null || lowers === null || lengths === false)
-        valid = false;
+    if (numbers === null || uppers === null || lowers === null || lengths === false) valid = false
 
-    if (numbers !== null && uppers !== null && lowers !== null && lengths)
-        valid = true;
+    if (numbers !== null && uppers !== null && lowers !== null && lengths) valid = true
 
-    return valid;
+    return valid
   }
 }
 
