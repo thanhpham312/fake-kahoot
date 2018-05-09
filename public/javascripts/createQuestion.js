cedba114d2c1d713570fec8f48a64b9cdb2c56dd
Shanyu Cui <kanoni4567@gmail.com>
2018-05-08 17:22:10 -0700

    added test for createQuestion()(failed)

--- public/javascripts/createQuestion.js
+++ public/javascripts/createQuestion.js
@@ -1,6 +1,10 @@
 let createQuestionWrap = document.getElementById('createQuestionWrap')
 let createQuestionOverlay = document.getElementById('createQuestionOverlay')
-
+let questionInput = document.getElementById('questionInput')
+let correctAnswerInput = document.getElementById("correctAnswerInput")
+let wrongAnswerInput1 = document.getElementById("wrongAnswerInput1")
+let wrongAnswerInput2 = document.getElementById("wrongAnswerInput2")
+let wrongAnswerInput3 = document.getElementById("wrongAnswerInput3")
 
 createQuestionOverlay.addEventListener('click', () => {
   createQuestionOverlay.style.backgroundColor = 'rgba(236, 239, 241, 0)'
@@ -22,3 +26,16 @@ let showCreateQuestionWindow = () => {
   }, 10)
 }
 
+let createQuestion = () => {
+  if (questionInput.value !== '' && correctAnswerInput.value !== '' && wrongAnswerInput1.value !== '' && wrongAnswerInput2.value !== '' && wrongAnswerInput3.value !== '') {
+    let xmlhttp = new XMLHttpRequest()
+    xmlhttp.open('POST', '/createQuestion', true)
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+    xmlhttp.onreadystatechange = () => {
+      if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        console.log(xmlhttp.responseText)
+      }
+    }
+    xmlhttp.send(`username=${loginInputUsername.value}&password=${loginInputPassword.value}`)
+  }
+}
