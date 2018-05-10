3af390d494a88f5cc64914a33f874c10972723e1
Shanyu Cui <kanoni4567@gmail.com>
2018-05-10 14:57:54 -0700

    a little bit of something doesn't really matter

--- models/userQuestions.js
+++ models/userQuestions.js
@@ -1,7 +1,7 @@
 const db = require('./database')
 
 let createQuestion = (Q,CA,WA1,WA2,WA3) => {
-	db.executeQuery('')
+	db.executeQuery(`INSERT INTO public."QUESTIONS"("QUESTION_CONTENT", "RIGHT_ANSWER", "WRONG_ANSWER1", "WRONG_ANSWER2", "WRONG_ANSWER3", "ACCOUNT_ID") VALUES (?, ?, ?, ?, ?, ?, ?);`)
 }
 
 module.exports = {
