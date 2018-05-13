d47838c3c344c985c310c31977edc88fe116d34f
Pedram Nazari <p.nazari2@gmail.com>
2018-05-12 17:05:15 -0700

    Created tests for validateUsername()  (#55)
    * add documentation
    
    * add JSDoc comments
    
    * tests
    
    * named test files properly
    
    * renamed test files so that jest can read them
    
    * AAAHHH
    
    * removed commented code
    
    * deleted commented code
    
    * moved tests to correct files; created test file templates
    
    * moved tests to correct files; created test file templates
    
    Create separate test files and add more documentation (#32)
    
    * add documentation
    
    * add JSDoc comments
    
    * add documentation for new routes
    
    * create test files and add more documentation
    
    * deleted old test file
    
    * Wrote tests for validate password, also fixed docstring for Account.validatePassword()
    
    * tests for validating username
    
    * fixed connection string
    
    * changed validatePassword to match mainrepo
    
    * deleted unneed test files, moved code from question.test.js to questions.test.js
    
    * standard style fix
    
    * deleted unused required modules in account.test.js
    
    * Created tests for validateUsername()
    Renamed Account.validatePassword() to Account.regexPassword() for better code readability (functionality is same)
    Refactored validateUsername() with reject error messages for "Bad Username" and "Username Not Found"
    Created Account.regexUsername() to validate username strings in the backend
    Created test block for Account.regexUsername()
    Created test block for Account.regexPassword()
    Removed unneeded requires in account.test.js
    
    * modified .gitignore to standard node settings
    
    * deleted console.log

--- app.js
+++ app.js
@@ -277,7 +277,7 @@ app.post('/validateusername', (request, response) => {
 
 app.post('/validatepassword', (request, response) => {
   let userAccount = new account.Account()
-  let result = userAccount.validatePassword(request.body.PASSWORD.toString())
+  let result = userAccount.regexPassword(request.body.PASSWORD.toString())
   if (result) {
     response.sendStatus(200)
   } else {
@@ -297,7 +297,7 @@ app.post('/register', (request, response) => {
   let userAccount = new account.Account()
 
   userAccount.validateUsername(USERNAME).then((result) => {
-    if (result && userAccount.validatePassword(PASSWORD) && PASSWORD === CPASSWORD) {
+    if (result && userAccount.regexPassword(PASSWORD) && PASSWORD === CPASSWORD) {
       console.log('validation passed')
       userAccount.register(USERNAME, PASSWORD).then((finalResult) => {
         response.send(finalResult)
@@ -317,13 +317,13 @@ app.post('/createQuestion', (request, response) => {
   let session_id = request.session.id.toString()
   let user_id = playingUsers[session_id].user.userID
 
-  userQuestions.createQuestion(QUESTION_CONTENT,RIGHT_ANSWER,WRONG_ANSWER1,WRONG_ANSWER2,WRONG_ANSWER3,user_id).then((result) => {
+  userQuestions.createQuestion(QUESTION_CONTENT, RIGHT_ANSWER, WRONG_ANSWER1, WRONG_ANSWER2, WRONG_ANSWER3, user_id).then((result) => {
     if (result) {
       response.sendStatus(200)
     } else {
       response.sendStatus(406)
     }
-  }) 
+  })
 })
 
 /**
--- tests/account.test.js
+++ tests/account.test.js
@@ -1,63 +1,117 @@
 /* eslint-env jest */
-const opentdb = require('../models/opentdb')
-const db = require('../models/database')
-const questions = require('../controllers/questions')
-const usersM = require('../models/users')
-const Account = require('../models/account')
-const promiseTest = opentdb.getQuestions()
-const invalidPromiseTestCatch = opentdb.getQuestions(
-  numberofQuestions = 1,
-  category = 1,
-  difficulty = 1,
-  questionType = 2
-)
+const db = require.requireActual('../models/database')
+const Account = require.requireActual('../models/account')
 
 beforeAll(() => {
-  db.executeQuery(`DELETE FROM public."ACCOUNTS" WHERE "USERNAME" = 'jestUser1';`);
-});
+  // db.executeQuery(`DELETE FROM public."ACCOUNTS" WHERE "USERNAME" = 'jestUser1';`)
+})
 
-const invalidPromiseTest = opentdb.getQuestions
+afterAll(() => {
+  return undefined
+})
+
+/**
+ * If beforeEach is inside a describe block, it runs for each test in the describe block.
+ */
+beforeEach(() => {
+  return undefined
+})
+
+/**
+ * If afterEach is inside a describe block, it runs for each test in the describe block.
+ */
+afterEach(() => {
+  return undefined
+})
 
 describe('Testing user registration/login', () => {
-  let accInst = new Account.Account();
-
-  test('Validating correct passwords', async () => {
-    let password = ['hello1P', '123Hello123', '!@#Test3213!@##']
-    for (let i = 0; i < password.length; i++) {
-      await expect(accInst.validatePassword(password[i])).toEqual(true)
-    }
-  })
-
-  test('Rejecting wrong passwords', async () => {
-    let wrongPassArr = ['hello', 'hello1', 'HELLO', '12312%$^#']
-    for (let i = 0; i < wrongPassArr.length; i++) {
-      await expect(accInst.validatePassword(wrongPassArr[i])).toEqual(false)
-    }
-  })
-
-  test('Validating username ', async () => {
-    let username = 'Hello1'
-    await expect(accInst.validateUsername(username)).resolves.toEqual(true)
-  })
+  let accInst = new Account.Account()
 
   test('Registering users work as expected', async () => {
     let username = 'jestUser1'
-    let password = 'jestUser'
-    await expect(accInst.register(username, password)).resolves.toEqual(true)
+    let password = 'Password1'
+    await expect(accInst.register(username, password)).resolves.toBeTruthy()
   })
 
   test('Login work as expected', async () => {
     let username = 'jestUser1'
-    let password = 'jestUser'
-    await expect(accInst.login(username, password)).resolves.toEqual(true)
-  })
-
-  
-  test('Validate password length, 5 characters password should be invalid', async () => {
-    let password = 'Helo1';
-    await expect(accInst.validatePassword(password)).toEqual(false);
+    let password = 'Password1'
+    await expect(accInst.login(username, password)).resolves.toBeTruthy()
   })
 })
 
+describe('validateUsername() tests', () => {
+  let accInst = new Account.Account()
 
+  it('should reject and throw error because of bad username inputs', async () => {
+    let badUsername = ['@@@13123asdasdASDADS', '0123456789012345678901234567890123456789', 'asd\n123\n', 'a']
+    for (let i = 0; i < badUsername.length; i++) {
+      await accInst.validateUsername(badUsername[i]).catch(error => {
+        expect(error.message).toBe('Bad Username')
+      })
+    }
+  })
 
+  it('should resolve truthy because valid usernames', async () => {
+    let goodUsername = ['as54d4535', 'DDsad3123', 'jestUser1', 'testUser']
+    for (let i = 0; i < goodUsername.length; i++) {
+      await accInst.validateUsername(goodUsername[i]).then(result => {
+        expect(result).toBeTruthy()
+      }).catch(error => {
+        expect(error.message).toBe('Username Not Found')
+      })
+    }
+  })
+})
+
+describe('regexUsername() method tests', () => {
+  let accInst = new Account.Account()
+
+  beforeEach(() => {
+    accInst = new Account.Account()
+  })
+
+  afterEach(function () {
+    accInst = undefined
+  })
+
+  test('test regex method for bad usernames', () => {
+    let badUsername = ['@@@13123asdasdASDADS', '0123456789012345678901234567890123456789', 'asd\n123\n', 'a']
+    for (let i = 0; i < badUsername.length; i++) {
+      expect(accInst.regexUsername(badUsername[i])).toBeFalsy()
+    }
+  })
+
+  test('test regex method for good usernames', () => {
+    let goodUsername = ['hello']
+    for (let i = 0; i < goodUsername.length; i++) {
+      expect(accInst.regexUsername(goodUsername[i])).toBeTruthy()
+    }
+  })
+})
+
+describe('regexPassword() method tests', () => {
+  let accInst = new Account.Account()
+
+  beforeEach(() => {
+    accInst = new Account.Account()
+  })
+
+  afterEach(function () {
+    accInst = undefined
+  })
+
+  it('Validating correct passwords', () => {
+    let password = ['hello1P', '123Hello123', '!@#Test3213!@##']
+    for (let i = 0; i < password.length; i++) {
+      expect(accInst.regexPassword(password[i])).toBeTruthy()
+    }
+  })
+
+  it('Rejecting wrong passwords', () => {
+    let wrongPassArr = ['hello', 'hello1', 'HELLO', '12312%$^#']
+    for (let i = 0; i < wrongPassArr.length; i++) {
+      expect(accInst.regexPassword(wrongPassArr[i])).toBeFalsy()
+    }
+  })
+})
