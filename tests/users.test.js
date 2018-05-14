36cf2d1778b3ab6598d808af5aa19a758902a8fc
Pedram Nazari <p.nazari2@gmail.com>
2018-05-13 18:52:06 -0700

    Mostly tests and fixed some functions (#60)
    * add documentation
    
    * tests
    
    * add documentation for new routes
    
    * named test files properly
    
    * renamed test files so that jest can read them
    
    * AAAHHH
    
    * create test files and add more documentation
    
    * account.js tests
    
    * fix database env url
    
    * add query to clear the database before running tests
    
    * fix spelling error
    
    * moved tests to correct files; created test file templates
    
    * add view for create question
    
    * JSDoc documentation for routes in app.js
    
    * Wrote tests for validate password, also fixed docstring for Account.validatePassword()
    
    * tests for validating username
    
    * changed validatePassword to match mainrepo
    
    * deleted unneed test files, moved code from question.test.js to questions.test.js
    
    * deleted unused required modules in account.test.js
    
    * Refactored "opentdb.test.js" tests to work properly
    Edited "userQuestions.test.js" to conform to JavaScript Standard Style
    Added a dummy test to "users.test.js"
    
    * Update LICENSE
    
    * Update LICENSE
    
    * editted users.test.js
    
    * Deprecated save/load JSON methods
    
    * Added yarn.lock for yarn package manager users
    
    * MINOR: Made changes to models so that tests can pass properly everytime
    
    * Deleted yarn.lock to fix heroku build failures
    
    * Edited Users class so that it creates the 'users_data.json' file inside the models folder
    
    * Delete console.log in executeQuery()
    
    * Refactored account.js and tests to work properly (I broke it) Also editted app.js to catch errors properly
    
    * Made createQuestion() more readable, changed account.test.js, delete main.test.js
    
    * Tests for executeQuery()
    Refactored Account.saveCurrentScore() with new timestamp and a readable SQL query
    Tests for Account.saveCurrentScore()
    Most DB queries now properly remove themselves after the database after tests have finished
    Better tests for Account.login() and Account.register()
    Refactored createQuestion() to be more readable and userID is converted to string so that there no SQL query errors

--- tests/users.test.js
+++ tests/users.test.js
@@ -6,8 +6,22 @@ beforeAll(() => {
 })
 
 afterAll(() => {
-  // need to remove last element from users_data.json
-});
+  return undefined
+})
+
+/**
+ * If beforeEach is inside a describe block, it runs for each test in the describe block.
+ */
+beforeEach(() => {
+  return undefined
+})
+
+/**
+ * If afterEach is inside a describe block, it runs for each test in the describe block.
+ */
+afterEach(() => {
+  return undefined
+})
 
 /**
  * If beforeEach is inside a describe block, it runs for each test in the describe block.
