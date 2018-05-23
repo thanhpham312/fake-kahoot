0466301113d5aad0c1744d20198205ba3fbe173e
thanhpham312 <tpham74@my.bcit.ca>
2018-05-23 14:17:41 -0700

    Added checks for difficulty ID when saving scores

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,14 +1,13 @@
 let scoreHistory = document.getElementById('scoreHistoryWrap')
 
 let displayPlayHistory = () => {
-    serverRequest('POST', '/playerhistory', '', (xmlhttp) => {
-        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-            scoreHistory.innerHTML = '<div id="scoreHistoryWrap">\n' +
-            '<h1>Score History</h1>\n' +
+  serverRequest('POST', '/playerhistory', '', (xmlhttp) => {
+    if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+      scoreHistory.innerHTML = '<h1>Score History</h1>\n' +
             xmlhttp.responseText + '\n</div>'
-            console.log(scoreHistory.innerHTML)
-        }
-    })
+      console.log(scoreHistory.innerHTML)
+    }
+  })
 }
 
 displayPlayHistory()
